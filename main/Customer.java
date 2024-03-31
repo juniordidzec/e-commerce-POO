bd2e56af38d7f63e56ca8be2aa9f6dcabd553fb2
Hélio Didzec Junior <81626842+juniordidzec@users.noreply.github.com>
2024-03-31 22:35:01 +0000

    Add new interfaces

--- main/Customer.java
+++ main/Customer.java
@@ -2,18 +2,46 @@ import java.util.Date;
 import java.util.ArrayList;
 import main.Items;
 
-public class Sale_list {
+public class Customer {
     public String address;
     public String payment_method;
     public String[4] cards;
     ArrayList<Items> cart = new ArrayList<>();
 
 
-    public Sale_list(String _Name, String _address, String _payment_method, String _card) {
+    public Customer(String _Name, String _address, String _payment_method, String _card) {
 		super(_Name);
         this.address = _address;
         this.payment_method = _payment_method;
         this.cards[0] = _card;
 	}
 
+    public edit_info(String _address, String _payment_method) {
+        this.address = _address;
+        this.payment_method = _payment_method;
+	}
+
+    public add_item_to_cart(Items _item) {
+        this.cart.add(_item);
+	}
+
+    public remove_item_to_cart(Items _item) {
+        this.cart.remove(_item);
+	}
+    
+    public clear_cart() {
+        this.cart.clear();
+	}
+
+    public pay() {
+        //Use this.cards to pay;
+	}
+
+    public add_card(int index, String _card) {
+        this.cards[index] = _card;
+	}
+
+    public remove_card(int index) {
+        this.cards[index] = null;
+	}
 }
--- main/Sale_list.java
+++ main/Sale_list.java
@@ -5,14 +5,14 @@ public class Sale_list {
     ArrayList<Items> Items_list = new ArrayList<>();
 
     public add_item(Items _item) {
-        Items_list.add(_item);
+        this.Items_list.add(_item);
 	}
 
     public remove_item(Items _item) {
-        Items_list.remove(_item);
+        this.Items_list.remove(_item);
 	}
 
     public buy_item(Items _item) {
-        _item.quantity.reduce_quantity(); // 
+        _item.quantity.reduce_quantity();
 	}
 }
